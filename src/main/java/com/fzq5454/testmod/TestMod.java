16e77797c68874dacd2d34ab399a7a4e5bc3bd01
Fzq5454 <fzq1989000@qq.com>
2025-10-24 13:01:57 +0800

    Update TestMod.java

--- src/main/java/com/fzq5454/testmod/TestMod.java
+++ src/main/java/com/fzq5454/testmod/TestMod.java
@@ -1,16 +1,12 @@
 package com.fzq5454.testmod;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import test.fabric.item.ModItems;
 
 public class TestMod implements ModInitializer {
     public static final String MODID = "test";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    
+
     @Override
     public void onInitialize() {
-        ModItems.register();
+        com.fzq5454.testmod.item.ModItems.register();
     }
 }
